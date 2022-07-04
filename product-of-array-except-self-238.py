c2989aa78ca57ff2efc42430c42a5040f9497f1a
karthikmurali60 <karthik.murali60@gmail.com>
2022-07-04 18:20:09 +0530

    Product of array except self - 238

---
+++ product-of-array-except-self-238.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length, rightArrProduct = len(nums), 1
+
+        answer = [1] * length
+
+        for i in range(1, length):
+            answer[i] = answer[i-1] * nums[i-1]
+        
+        for i in range(length - 1, -1, -1):
+            answer[i] = answer[i] * R
+            R *= nums[i]
+        
+        return answer
