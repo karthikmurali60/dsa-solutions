9d9d0dd6087c0849d52bd9ea2e4f78d4fa712fe3
Karthik Murali <karthik.murali60@gmail.com>
2022-10-24 21:14:09 -0700

    Time: 62 ms (95.70%), Space: 14 MB (75.30%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,23 @@
 class Solution:
-    def findCombination(self, index, candidates, target, ans, ds):
+    def dfs(self, index, candidates, result, ds, target):
         if index == len(candidates):
             if target == 0:
-                ans.append(list(ds))
+                result.append(list(ds))
             return
         
+        # pick the element at the current index
         if candidates[index] <= target:
             ds.append(candidates[index])
-            self.findCombination(index, candidates, target - candidates[index], ans, ds)
+            self.dfs(index, candidates, result, ds, target - candidates[index])
             ds.pop()
         
-        self.findCombination(index + 1, candidates, target, ans, ds)
+        # do not pick the element at the current index
+        self.dfs(index + 1, candidates, result, ds, target)
+        
         
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
+        result = []
         
-        self.findCombination(0, candidates, target, ans, [])
+        self.dfs(0, candidates, result, [], target)
         
-        return ans
+        return result
