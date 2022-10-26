c272da1b019fa2d0022b92a28f4a605be58389d2
Karthik Murali <karthik.murali60@gmail.com>
2022-10-25 23:20:34 -0700

    Time: 69 ms (15.18%), Space: 13.9 MB (65.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,36 @@
 class Solution:
-    def backtrack(self, index, nums, dp):
-        if index == 0:
-            return nums[index]
-        if index < 0:
-            return 0
-        if dp[index] != -1:
-            return dp[index]
+#     def backtrack(self, index, nums, dp):
+#         if index == 0:
+#             return nums[index]
+#         if index < 0:
+#             return 0
+#         if dp[index] != -1:
+#             return dp[index]
         
-        pick = nums[index] + self.backtrack(index - 2, nums, dp)
-        notPick = self.backtrack(index - 1, nums, dp)
+#         pick = nums[index] + self.backtrack(index - 2, nums, dp)
+#         notPick = self.backtrack(index - 1, nums, dp)
         
-        dp[index] = max(pick, notPick)
+#         dp[index] = max(pick, notPick)
         
-        return max(pick, notPick)
+#         return max(pick, notPick)
     
     def rob(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
-        return self.backtrack(len(nums) - 1, nums, dp)
+        if len(nums) == 1:
+            return nums[0]
+        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        
+        for i in range(1, len(nums)):
+            pick = nums[i] 
+            if i > 1:
+                pick += dp[i - 2]
+                
+            notPick = dp[i - 1]
+            
+            dp[i] = max(pick, notPick)
+            
+        return dp[len(nums) - 1]
+        
+        
