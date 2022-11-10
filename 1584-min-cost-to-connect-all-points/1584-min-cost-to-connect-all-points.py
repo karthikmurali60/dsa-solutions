b076808c8ab9f7b39fa1a6a92360fdfab63cf3e9
Karthik Murali <karthik.murali60@gmail.com>
2022-11-10 12:37:30 -0800

    Time: 5517 ms (35.42%), Space: 94.4 MB (58.07%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,38 +1,59 @@
+class UnionFind:
+    def __init__(self, size: int) -> None:
+        self.group = [0] * size
+        self.rank = [0] * size
+        
+        for i in range(size):
+            self.group[i] = i
+      
+    def find(self, node: int) -> int:
+        if self.group[node] != node:
+            self.group[node] = self.find(self.group[node])
+        return self.group[node]
+
+    def join(self, node1: int, node2: int) -> bool:
+        group1 = self.find(node1)
+        group2 = self.find(node2)
+        
+        # node1 and node2 already belong to same group.
+        if group1 == group2:
+            return False
+
+        if self.rank[group1] > self.rank[group2]:
+            self.group[group2] = group1
+        elif self.rank[group1] < self.rank[group2]:
+            self.group[group1] = group2
+        else:
+            self.group[group1] = group2
+            self.rank[group2] += 1
+
+        return True
+    
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        pq = []
         n = len(points)
+        count, cost = 0, 0
+        uf = UnionFind(n)
         
-        adjList = {}
-        
-        for i in range(n):
-            adjList[i] = []
-            
         for i in range(n):
             x1, y1 = points[i]
             for j in range(i + 1, n):
                 x2, y2 = points[j]
                 
-                distance = abs(x1 - x2) + abs(y1 - y2)
+                manDist = abs(x1 - x2) + abs(y1 - y2)
                 
-                adjList[i].append([distance, j])
-                adjList[j].append([distance, i])
+                pq.append([manDist, i, j])
                 
+        pq.sort()
         
-        visited = set()
-        minHeap = [[0,0]]
-        res = 0
-        
-        while len(visited) < n:
-            cost, i = heapq.heappop(minHeap)
+        for dist, p1, p2 in pq:
+            if uf.join(p1, p2):
+                count += 1
+                cost += dist
+                
+                if count == n - 1:
+                    break
             
-            if i in visited:
-                continue
+        return cost
             
-            res += cost
-            visited.add(i)
-            
-            for cost, adj in adjList[i]:
-                if adj not in visited:
-                    heapq.heappush(minHeap, [cost, adj])
-        
-        return res
