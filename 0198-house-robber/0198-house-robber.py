32e96195557b9b263a19ee39b9f863d991e71e2b
Karthik Murali <karthik.murali60@gmail.com>
2022-11-17 10:49:38 -0800

    Time: 63 ms (36.35%), Space: 13.9 MB (19.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,23 @@
 class Solution:
+    def backtrack(self, nums, index, dp):
+        if index == 0:
+            return nums[index]
+        
+        if index < 0:
+            return 0
+        
+        if dp[index] != -1:
+            return dp[index]
+        
+        pick = nums[index] + self.backtrack(nums, index - 2, dp)
+        notPick = self.backtrack(nums, index - 1, dp)
+        
+        dp[index] = max(pick, notPick)
+        
+        return max(pick, notPick)
+    
+    
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        dp = [-1] * len(nums)
         
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = nums[1]
-        
-        for i in range(1, len(nums)):
-            pick = nums[i] 
-            if i > 1:
-                pick += dp[i - 2]
-                
-            notPick = dp[i - 1]
-            
-            dp[i] = max(pick, notPick)
-            
-        return dp[len(nums) - 1]
-        
-        
+        return self.backtrack(nums, len(nums) - 1, dp)
