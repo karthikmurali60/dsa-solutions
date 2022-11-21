852aae1e6819787674e7acd35f7e83ca64f6bd1a
Karthik Murali <karthik.murali60@gmail.com>
2022-11-21 13:27:24 -0800

    Time: 63 ms (93.15%), Space: 14 MB (30.57%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,20 @@
 class Solution:
-    def backtrack(self, index, candidates, target, result, temp):
+    def backtrack(self, index, candidates, target, result, ds):
         if index == len(candidates):
             if target == 0:
-                result.append(list(temp))
+                result.append(list(ds))
             
             return
         
+        # pick the element at the current index.
         if target >= candidates[index]:
-            temp.append(candidates[index])
-            self.backtrack(index, candidates, target - candidates[index], result, temp)
-            temp.pop()
-            
-        self.backtrack(index + 1, candidates, target, result, temp)
+            ds.append(candidates[index])
+            self.backtrack(index, candidates, target - candidates[index], result, ds)
+            ds.pop()
         
+        # do not pick the element at the current index.
+        self.backtrack(index + 1, candidates, target, result, ds)
+    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
         
