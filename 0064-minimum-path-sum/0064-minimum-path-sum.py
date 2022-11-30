0de3b34cb44af125a125bc92ad20da8dab3f9342
Karthik Murali <karthik.murali60@gmail.com>
2022-11-29 16:21:12 -0800

    Time: 261 ms (30.54%), Space: 16.1 MB (17.54%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -8,14 +8,15 @@ class Solution:
         
         if dp[i][j] != -1:
             return dp[i][j]
-        
+            
         left = grid[i][j] + self.backtrack(i - 1, j, grid, dp)
         up = grid[i][j] + self.backtrack(i, j - 1, grid, dp)
         
         dp[i][j] = min(left, up)
         
-        return min(left, up)        
+        return min(left, up)
         
+    
     def minPathSum(self, grid: List[List[int]]) -> int:
         dp = [[-1] * len(grid[0]) for i in range(len(grid))]
         
