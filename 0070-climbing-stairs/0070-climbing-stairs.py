0a86d5e4c1adeb3e4cd541e461bd3bb6b88cd653
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-21 20:33:42 -0800

    Time: 37 ms (28.16%), Space: 13.8 MB (48.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,22 @@
 class Solution:
-    def backtrack(self, index, dp):
-        if index < 0:
-            return 0
+    def dfs(self, index, dp):
         if index == 0:
             return 1
         
         if dp[index] != -1:
             return dp[index]
         
-        dp[index] = self.backtrack(index - 2, dp) + self.backtrack(index - 1, dp)
+        oneStep = self.dfs(index - 1, dp)
         
-        return dp[index]
-    
+        twoStep = 0
+        if index > 1:
+            twoStep = self.dfs(index - 2, dp)
+            
+        dp[index] = oneStep + twoStep
+        
+        return oneStep + twoStep
+        
     def climbStairs(self, n: int) -> int:
         dp = [-1] * (n + 1)
         
-        return self.backtrack(n, dp)
+        return self.dfs(n, dp)
