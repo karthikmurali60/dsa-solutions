4fea44e92a8c02ecae50d36b48609480ddba0343
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-25 13:22:14 -0800

    Time: 38 ms (74.63%), Space: 14 MB (90.54%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,20 +1,21 @@
 class Solution:
-    def backtrack(self, index, nums, result, ds):
-        result.append(list(ds))
+    def dfs(self, index, nums, ds, ans):
+        ans.append(list(ds))
         
         for i in range(index, len(nums)):
-            if i > index and nums[i] == nums[i - 1]:
+            if i != index and nums[i] == nums[i - 1]:
                 continue
                 
             ds.append(nums[i])
-            self.backtrack(i + 1, nums, result, ds)
+            self.dfs(i + 1, nums, ds, ans)
             ds.pop()
-            
-        
+    
+    
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        ans = []
+        
         nums.sort()
         
-        self.backtrack(0, nums, result, [])
+        self.dfs(0, nums, [], ans)
         
-        return result
+        return ans
