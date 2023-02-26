b2812ab7eb23d74db51412203409643fc593601c
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-26 13:05:58 -0800

    Time: 2203 ms (36.74%), Space: 20.2 MB (83.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,29 +1,33 @@
 class Solution:
-    def backtrack(self, i, j1, j2, rows, cols, grid, dp):
-        if j1 < 0 or j2 < 0 or j1 >= cols or j2 >= cols:
+    def dfs(self, i, j1, j2, m, n, grid, dp):
+        if j1 < 0 or j1 > n - 1 or j2 < 0 or j2 > n - 1:
             return -float('inf')
-
-        if i == rows - 1:
+        
+        if i == m - 1:
             if j1 == j2:
                 return grid[i][j1]
             else:
                 return grid[i][j1] + grid[i][j2]
-        
+            
         if dp[i][j1][j2] != -1:
             return dp[i][j1][j2]
-
+        
         maxSum = -float('inf')
-
+        
         for dj1 in range(-1, 2):
             for dj2 in range(-1, 2):
                 value = 0
+                
+                nj1 = j1 + dj1
+                nj2 = j2 + dj2
+                
                 if j1 == j2:
                     value = grid[i][j1]
                 else:
                     value = grid[i][j1] + grid[i][j2]
-
-                value += self.backtrack(i + 1, j1 + dj1, j2 + dj2, rows, cols, grid, dp)
-
+                
+                value += self.dfs(i + 1, nj1, nj2, m, n, grid, dp)
+                
                 maxSum = max(maxSum, value)
 
         dp[i][j1][j2] = maxSum
@@ -31,9 +35,8 @@ class Solution:
         return maxSum
 
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        m, n = len(grid), len(grid[0])
         
-        dp = [[[-1]*cols for _ in range(cols)] for __ in range(rows)]
+        dp = [[[-1] * n for _ in range(n)] for __ in range(m)]
         
-        return self.backtrack(0, 0, cols - 1, rows, cols, grid, dp)
+        return self.dfs(0, 0, n - 1, m, n, grid, dp)
