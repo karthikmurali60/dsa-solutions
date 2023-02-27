3c21f1e09b79737545cf03b97c1f91760ef8701f
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-27 11:54:27 -0800

    Time: 220 ms (65.01%), Space: 18.3 MB (21.04%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,34 +1,34 @@
 class Solution:
-    def dfs(self, i, j, rows, cols, grid, vis, island, br, bc):
+    def dfs(self, i, j, bi, bj, m, n, grid, vis, path):
         vis[i][j] = 1
-        island.append(tuple([i - br, j - bc]))
+        
+        path.append(tuple[i - bi, j - bj])
         
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         
-        for dr, dc in directions:
-            nr, nc = dr + i, dc + j
+        for di, dj in directions:
+            ni, nj = i + di, j + dj
             
-            if nr < 0 or nr > rows - 1 or nc < 0 or nc > cols - 1 or vis[nr][nc] == 1 or grid[nr][nc] != 1:
+            if ni < 0 or ni >= m or nj < 0 or nj >= n or grid[ni][nj] == 0 or vis[ni][nj] == 1:
                 continue
-            
-            self.dfs(nr, nc, rows, cols, grid, vis, island, br, bc)
-            
+                
+            self.dfs(ni, nj, bi, bj, m, n, grid, vis, path)    
+    
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         count = 0
+        m, n = len(grid), len(grid[0])
         
-        rows, cols = len(grid), len(grid[0])
+        vis = [[0] * n for _ in range(m)]
         
-        vis = [[0] * cols for i in range(rows)]
-        distinctIslands = set()
+        s = set()
         
-        for i in range(rows):
-            for j in range(cols):
-                if vis[i][j] != 1 and grid[i][j] == 1:
-                    island = []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not vis[i][j]:
+                    path = []
                     
-                    self.dfs(i, j, rows, cols, grid, vis, island, i, j)
+                    self.dfs(i, j, i, j, m, n, grid, vis, path)
                     
-                    distinctIslands.add(tuple(island))
-        
-        return len(distinctIslands)
-        
+                    s.add(tuple(path))
+                    
+        return len(s)
