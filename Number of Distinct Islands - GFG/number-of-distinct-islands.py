c2b4aedad5fe9a44d580cb0a41006073d5131013
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-28 12:52:46 -0800

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.py
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.py
@@ -3,22 +3,23 @@
 import sys
 from typing import List
 sys.setrecursionlimit(10**8)
+
+
 class Solution:
-    def dfs(self, i, j, grid, vis, path, bi, bj):
+    def dfs(self, i, j, bi, bj, m, n, grid, vis, islands):
         vis[i][j] = 1
+        islands.append(tuple([i - bi, j - bj]))
         
-        path.append(tuple([i - bi, j - bj]))
-        
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
         
         for di, dj in directions:
             ni, nj = i + di, j + dj
             
-            if ni < 0 or ni >= len(grid) or nj < 0 or nj >= len(grid[0]) or vis[ni][nj] or grid[ni][nj] == 0:
+            if ni < 0 or ni > m - 1 or nj < 0 or nj > n - 1 or grid[ni][nj] == 0 or vis[ni][nj] == 1:
                 continue
             
-            self.dfs(ni, nj, grid, vis, path, bi, bj)
-
+            self.dfs(ni, nj, bi, bj, m, n, grid, vis, islands)
+    
     def countDistinctIslands(self, grid : List[List[int]]) -> int:
         # code here
         
@@ -31,11 +32,14 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1 and not vis[i][j]:
-                    path = []
-                    self.dfs(i, j, grid, vis, path, i, j)
-                    s.add(tuple(path))
+                    islands = []
+                    
+                    self.dfs(i, j, i, j, m, n, grid, vis, islands)
+                    
+                    s.add(tuple(islands))
                     
         return len(s)
+
 
 #{ 
  # Driver Code Starts
