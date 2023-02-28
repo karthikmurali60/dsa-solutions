ef2a438166b97978cb6da28a9da47a252f13bff2
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-02-28 11:34:51 -0800

    Time: 304 ms (50.09%), Space: 17.4 MB (48.62%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,31 +3,32 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
+        
         queue = []
+        
+        m, n = len(rooms), len(rooms[0])
+        
+        vis = [[0] * n for _ in range(m)]
+        
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         
-        rows = len(rooms)
-        cols = len(rooms[0])
-        vis = [[0] * cols for i in range(rows)]
-        
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(m):
+            for j in range(n):
                 if rooms[i][j] == 0:
                     queue.append([i, j, 0])
                     vis[i][j] = 1
-        
-        while queue:
+                    
+        while len(queue) != 0:
             r, c, d = queue.pop(0)
             
             for dr, dc in directions:
-                nr, nc = dr + r, dc + c
+                nr, nc = r + dr, c + dc
                 
-                if nr < 0 or nr > rows - 1 or nc < 0 or nc > cols - 1 or rooms[nr][nc] == -1 or rooms[nr][nc] == 0 or vis[nr][nc] == 1:
+                if nr < 0 or nr > m - 1 or nc < 0 or nc > n - 1 or rooms[nr][nc] == -1 or rooms[nr][nc] == 0 or vis[nr][nc] == 1:
                     continue
                     
                 vis[nr][nc] = 1
                 rooms[nr][nc] = d + 1
-                queue.append([nr, nc, d + 1])
                 
-                    
-                
+                queue.append([nr, nc, d + 1])    
+        
