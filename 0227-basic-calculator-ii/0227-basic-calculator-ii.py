e35d50339cba6c307922ac9cdd06be6208267cc5
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-03-06 16:53:56 -0800

    Time: 208 ms (41.42%), Space: 16.9 MB (49.66%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,40 +1,40 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        curr = 0
-        operator = "+"
-
-        if not s:
-            return 0
-
-        operators = {'+','-','*','/'}
-        nums = set(str(x) for x in range(0,10))
-
-        for i in range(0,len(s)):
-            char = s[i]
-
-            if char in nums:
-                curr = curr * 10 + int(char)
-
-            if char in operators or i == len(s)-1:
-                if operator == '+':
-                    stack.append(curr)
-
-                elif operator == '-':
-                    stack.append(-curr)
-
-                elif operator == '*':
-                    stack[-1] *= curr
-
-                elif operator == '/':
-                    stack[-1] = int(stack[-1] / curr)
-
-                curr = 0
-                operator = char
-
-        resultSum = 0
-        for num in stack:
-            resultSum += num
-            
-        return resultSum
+        currNumber = 0
         
+        currOperator = "+"
+        
+        stack = []
+        
+        numbers = [str(i) for i in range(0, 10)]
+        
+        operators = ["+", "-", "*", "/"]
+        
+        for i in range(len(s)):
+            currChar = s[i]
+            
+            if currChar in numbers:
+                currNumber = currNumber * 10 + int(currChar)
+                
+            if currChar in operators or i == len(s) - 1:
+                if currOperator == "+":
+                    stack.append(currNumber)
+                    
+                elif currOperator == "-":
+                    stack.append(-currNumber)
+                    
+                elif currOperator == "*":
+                    stack[-1] = stack[-1] * currNumber
+                    
+                elif currOperator == '/':
+                    stack[-1] = int(stack[-1] / currNumber)
+                    
+                currNumber = 0
+                
+                currOperator = currChar
+                
+        result = 0
+        for num in stack:
+            result += num
+            
+        return result
