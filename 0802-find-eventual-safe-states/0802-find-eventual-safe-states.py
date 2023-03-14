25b8b295e745e08aba1a9d40c24fab84bbcd3449
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-03-14 14:00:14 -0700

    Time: 710 ms (42.97%), Space: 21.9 MB (36.60%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,30 +1,69 @@
 class Solution:
-    def dfs(self, index, graph, vis):
-        vis[index] = 2
+#     def dfs(self, index, graph, vis):
+#         vis[index] = 2
         
-        for node in graph[index]:
-            if vis[node] == 0:
-                if self.dfs(node, graph, vis):
-                    return True
-            elif vis[node] == 2:
-                return True
+#         for node in graph[index]:
+#             if vis[node] == 0:
+#                 if self.dfs(node, graph, vis):
+#                     return True
+#             elif vis[node] == 2:
+#                 return True
             
-        vis[index] = 1
-        return False    
+#         vis[index] = 1
+#         return False    
     
-    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        vis = [0] * len(graph)
+#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+#         vis = [0] * len(graph)
         
-        ans = []
+#         ans = []
+        
+#         for i in range(len(graph)):
+#             if vis[i] == 0:
+#                 self.dfs(i, graph, vis)
+                
+#         for i in range(len(vis)):
+#             if vis[i] == 1:
+#                 ans.append(i)
+        
+#         ans.sort()
+        
+#         return ans
+
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        revGraph = {}
         
         for i in range(len(graph)):
-            if vis[i] == 0:
-                self.dfs(i, graph, vis)
+            revGraph[i] = []
+            
+        for node, neighbours in enumerate(graph):
+            for neighbour in neighbours:
+                revGraph[neighbour].append(node)
                 
-        for i in range(len(vis)):
-            if vis[i] == 1:
-                ans.append(i)
+        indegree = [0] * len(graph)
         
-        ans.sort()
+        for node, neighbours in revGraph.items():
+            for neighbour in neighbours:
+                indegree[neighbour] += 1
+                
+        queue = []
         
-        return ans
+        for i in range(len(graph)):
+            if indegree[i] == 0:
+                queue.append(i)
+                
+        safe = []
+        while queue:
+            ele = queue.pop(0)
+            
+            safe.append(ele)
+            
+            for adjNode in revGraph[ele]:
+                indegree[adjNode] -= 1
+                
+                if indegree[adjNode] == 0:
+                    queue.append(adjNode)
+                    
+        safe.sort()
+        
+        return safe
+        
