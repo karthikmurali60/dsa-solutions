e838c50d158a4ec68bde40f3092d9a24bfba91d5
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-03-15 15:35:55 -0700

    Time: 304 ms (41.73%), Space: 17.7 MB (7.85%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,32 +1,32 @@
 class Solution:
-    def dfs(self, i, j, prevHeight, heights, vis):
-        if (i, j) in vis or i < 0 or i > len(heights) - 1 or j < 0 or j > len(heights[0]) - 1 or heights[i][j] < prevHeight:
+    def dfs(self, row, col, prevHeight, heights, vis):
+        if row < 0 or row > len(heights) - 1 or col < 0 or col > len(heights[0]) - 1 or (row, col) in vis or heights[row][col] < prevHeight:
             return
         
-        vis.add((i, j))
+        vis.add((row, col))
         
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         
         for dr, dc in directions:
-            ni, nj = i + dr, j + dc
+            nr, nc = row + dr, col + dc
             
-            self.dfs(ni, nj, heights[i][j], heights, vis)
+            self.dfs(nr, nc, heights[row][col], heights, vis)
+            
     
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
-        
         pac, atl = set(), set()
         
-        for i in range(cols):
-            self.dfs(0, i, heights[0][i], heights, pac)
-            self.dfs(rows - 1, i, heights[rows - 1][i], heights, atl)
+        rows, cols = len(heights), len(heights[0])
+        
+        for j in range(cols):
+            self.dfs(0, j, 0, heights, pac)
+            self.dfs(rows - 1, j, 0, heights, atl)
             
         for i in range(rows):
-            self.dfs(i, 0, heights[i][0], heights, pac)
-            self.dfs(i, cols - 1, heights[i][cols - 1], heights, atl)
+            self.dfs(i, 0, 0, heights, pac)
+            self.dfs(i, cols - 1, 0, heights, atl)
             
         res = []
-        
         for i in range(rows):
             for j in range(cols):
                 if (i, j) in pac and (i, j) in atl:
