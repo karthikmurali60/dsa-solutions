7e6f0e2d5a17c642b6f29f2be9416639aa9372ee
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-03-15 18:18:54 -0700

    Time: 4916 ms (8.20%), Space: 19.2 MB (55.52%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -17,11 +17,12 @@ class Solution:
             if loc == stones[n - 1]:
                 return True
             
-            for i in range(jump - 1, jump + 2):
-                if i <= 0:
-                    continue
-                elif loc + i in stones:
-                    queue.append((loc + i, i))
-                    
+            if loc < stones[-1]:
+                for i in range(jump - 1, jump + 2):
+                    if i <= 0:
+                        continue
+                    elif loc + i in stones:
+                        queue.append((loc + i, i))
+
         return False
             
