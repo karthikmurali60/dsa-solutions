b01d7c5903343d6ba29078bf8056391f1068c904
Karthik Muraliprasad <karthik.murali60@gmail.com>
2023-09-11 19:25:19 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..py
@@ -0,0 +1,33 @@
+#User function Template for python3
+class Solution:
+    def minDeletions(self, Str, n): 
+        #code here
+        revS = Str[::-1]
+        
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if Str[i - 1] == revS[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    
+        return n - dp[n][n]
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+       
+        N = int(input())
+        Str = input().strip()
+        ob = Solution()
+        ans = ob.minDeletions(Str, N)
+        print(ans)
+# } Driver Code Ends
